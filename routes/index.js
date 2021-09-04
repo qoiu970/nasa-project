582aa4594280a554ca78e68f1fee7df38233f8b1
qoiu970 <qoiu1970@gmail.com>
2021-09-04 16:53:17 +0800

    [upload] nasa-project

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Nasa_project' });
 });
 
+router.get('/added', function(req, res, next) {
+  let  a = parseInt(req.query.a),
+       b = parseInt(req.query.b);
+  res.send(a+ "+"+b+"="+(a+b));
+});
 module.exports = router;
